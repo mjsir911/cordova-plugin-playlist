d8d87da24bfa150594bb9ebc9ce4478be9ad2433
Marco Sirabella <marco@sirabella.org>
2019-05-22 14:27:01 -0400

    Remove const because QList really wants to assign

--- src/ubuntu/AudioTrack.h
+++ src/ubuntu/AudioTrack.h
@@ -11,41 +11,41 @@ struct AudioTrack {
    * This item is a streaming asset. Make sure this is set to true for stream URLs,
    * otherwise you will get odd behavior when the asset is paused.
    */
-  const bool isStream;
+  bool isStream;
 
   /**
    * trackId is optional and if not passed in, an auto-generated UUID will be used.
    */
-  const QString trackId;
+  QString trackId;
 
   /**
    * URL of the asset; can be local, a URL, or a streaming URL.
    * If the asset is a stream, make sure that isStream is set to true,
    * otherwise the plugin can't properly handle the item's buffer.
    */
-  const QUrl assetUrl;
+  QUrl assetUrl;
 
   /**
    * The local or remote URL to an image asset to be shown for this track.
    * If this is null, the plugin's default image is used.
    * This field is not used on iOS (yet)
    */
-  const QUrl albumArt;
+  QUrl albumArt;
 
   /**
    * The track's artist
    */
-  const QString artist;
+  QString artist;
 
   /**
    * Album the track belongs to
    */
-  const QString album;
+  QString album;
 
   /**
    * Title of the track
    */
-  const QString title;
+  QString title;
 
 
 
